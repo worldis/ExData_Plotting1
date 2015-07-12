a4c47a3ab1bf43fec3e0b35f9d1a032c63fcda3f
MingJie <eudaimonia.ye@yahoo.com>
2015-07-12 16:58:17 +0800

    update

---
+++ plot1.R
@@ -0,0 +1,3 @@
+hpc <- file("household_power_consumption.txt")
+ht <- read.table(text = grep("^[1,2]/2/2007", readLines(hpc), value = TRUE), col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep = ";", header = TRUE)
+hist(ht$Global_active_power, col = "red", main = paste("Global Active Power"), xlab = "Global Active Power (kilowatts)")
